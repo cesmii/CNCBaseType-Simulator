7cf95e935dd709ca21cfe17ccb95f77bc1fc5c98
jwise-mfg <jonathan.wise@cesmii.org>
2023-11-20 12:34:06 -0500

    fix service install bugs

--- start-sim.sh
+++ start-sim.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if [ $# -eq 0 ]; then
-    ARG=1
+    ARG=-o 1
 else
-    ARG=$1
+    ARG="$@"
 fi
-ARG="-o $ARG"
 echo Launching CNCBaseType Simulator with argument: $ARG
 echo
 /usr/bin/python3 $SCRIPT_DIR/sim.py $ARG
